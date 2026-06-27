f9ed0d1e8ec39b7f2e0fbb5bded530137f86f768
Carlo Huesca Spairani <carlo.huesca@ua.es>
2026-06-27 19:59:45 +0200

    UPDATE

---
+++ _next/static/chunks/app/(public)/newsletter/confirmacion/page-ee7e2513d8c6d863.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[730],{1442:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3078,23))}},e=>{e.O(0,[78,601,266,358],()=>e(e.s=1442)),_N_E=e.O()}]);
